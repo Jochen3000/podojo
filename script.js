e11fd993e4bea4509575b04591e45b0fe349b5b2
jochen ade <jochen.ade@hotmail.com>
2022-09-09 14:35:33 +0200

    switch to svg logo

--- script.js
+++ script.js
@@ -34,38 +34,7 @@ for (let i = 0; i < items.length; i++) {
     io.observe(items[i]);
 }
 
-
-// < !--Add classes for fade in old script -->
-//     <script>
-//         (function(){
-//             document.addEventListener('DOMContentLoaded', () => {
-//                 const idFadeEls = [
-//                     // List id to apply
-//                     '#block-61ae3e6744a84b3479f4aca3',
-//                     '#block-61ae3ec4554a7221c8a8130c',
-//                     '#block-631356e8e986304147f9f7ff',
-//                     '#block-63135c2a2a04d0e60a0df2f1',
-//                     '#block-63135c3a36c665b182fa0099',
-//                     '#block-63135c475c19ad3fb19c387b'
-//                 ];
-
-//                 let observer = new IntersectionObserver((entries, observer) => {
-//                     entries.forEach(entry => {
-//                         if (entry.isIntersecting) {
-//                             entry.target.classList.add("fadeIn");
-//                         } else entry.target.classList.add("postscroll");
-//                     });
-//                 }, {
-//                     threshold: 1
-//                 });
-
-//                 idFadeEls.forEach(id => {
-//                     const element = document.querySelector(id);
-//                     if (element) {
-//                         element.classList.add("preFade");
-//                         observer.observe(element);
-//                     }
-//                 })
-//             });
-//   })()
-//     </script>
+// Add svg logo
+const logoPos = document.querySelectorAll('.header-title-logo a');
+logoPos[0].innerHTML = '<img src="/img/logo-podojo-black.svg" alt="podojo">';
+logoPos[1].innerHTML = '<img src="/img/logo-podojo-black.svg" alt="podojo">';
